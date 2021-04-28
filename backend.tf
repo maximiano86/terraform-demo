c500e5241f4d083eb71108a601c509003675a431
maximiano86 <maximiano.alejandro@gmail.com>
2021-04-28 14:08:16 -0500

    add: remote backend

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "digital-demo-s3"
-    key    = "digital-remote-state.tfstate"
+    key    = "digital-remote-demo.tfstate"
     region = "us-east-1"
     # For locking create DynamoDB table with LockID primary key
     # dynamodb_table = "table_name"
